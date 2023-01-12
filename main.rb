6bbe6a340b1c1e7373fb64e9072d000c1e4d8878
Lance <111308100+LanceScheffer@users.noreply.github.com>
2023-01-12 03:19:34 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,15 @@
+# Exercise 1.2: Use escaping to output single quotes in a string
+# Create a ruby file called exercise-1.2.rb.
+
+# Output 'Single Quotes' is actually my favorite movie. It's great. to the console (hint: backslashes)
+
+variable = "'Single Quotes' is actually my favorite movie. It's great!"
+puts variable
+
+# Using backslashes to escape quotes in double quoted strings
+another_variable = "\'Single Quotes\' is actually my favorite movie. It's great!"
+puts another_variable
+
+# Using backslashes to escape quotes in single quoted strings
+third_variable = '\'Single Quotes\' is actually my favorite movie. It is great!'
+puts third_variable
